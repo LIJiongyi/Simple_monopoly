209f41d3c7c1be72adc3ba5bf4dc69d5ea7ef88b
ljy <1924148097@qq.com>
2024-10-30 17:54:47 +0800

    my-> self

--- monopoly.py
+++ monopoly.py
@@ -3,107 +3,107 @@ import sys
 
 # Players stats
 class Player:
-    def __init__(my, name): # previous "participant" switch to init
-        my.money = 1500
-        my.name = name
-        my.position = 0
-        my.properties = []
-        my.jail = False
-        my.jailturns = 0
+    def __init__(self, name): # previous "participant" switch to init
+        self.money = 1500
+        self.name = name
+        self.position = 0
+        self.properties = []
+        self.jail = False
+        self.jailturns = 0
 
-    def move(my, step):
-        my.position = (my.position + step) % 20
-        current_slot = board.myposition(my.position) 
-        current_slot.effect(my)
+    def move(self, step):
+        self.position = (self.position + step) % 20
+        current_slot = board.selfposition(self.position) 
+        current_slot.effect(self)
 
-    def purchase(my, property):
-        if my.money >= property.price and property.owner is None:
-            my.money = my.money - property.price
-            my.properties.append(property)
-            property.owner = my
+    def purchase(self, property):
+        if self.money >= property.price and property.owner is None:
+            self.money = self.money - property.price
+            self.properties.append(property)
+            property.owner = self
 
-    def payrent(my, property):
-        if property.owner != my:
+    def payrent(self, property):
+        if property.owner != self:
             rent = property.rent
-            my.money -= rent
+            self.money -= rent
             property.owner.money += rent
 
-    def injail(my):
-        my.jail = True
-        my.position = 6
-        my.jailturns = 0
+    def injail(self):
+        self.jail = True
+        self.position = 6
+        self.jailturns = 0
 
-    def outjail(my):
-        my.jail = False
-        my.jailturns = 0
+    def outjail(self):
+        self.jail = False
+        self.jailturns = 0
     
 class Property:
-    def propinfo(my, name, rent, price):
-        my.name = name
-        my.rent = rent
-        my.price = price
-        my.owner = None
+    def propinfo(self, name, rent, price):
+        self.name = name
+        self.rent = rent
+        self.price = price
+        self.owner = None
 
 class Slot:
-    def slotinfo(my, name, kind):
-        my.name = name
-        my.kind = kind
+    def slotinfo(self, name, kind):
+        self.name = name
+        self.kind = kind
 
-    def effect(my, player):
+    def effect(self, player):
         pass
 
 #add 1500 to player toyal money
 class Goslot(Slot):
-    def effect(player, my):
+    def effect(player, self):
         player.money += 1500
 
 #
 class Property_Slot(Slot):
-    def propertyactivity(my, rent, name, price):
+    def propertyactivity(self, rent, name, price):
         super().slotinfo(name, "property")
-        my.property = my.property(name, price, rent)
+        self.property = self.property(name, price, rent)
 
-    def effect(my, player):
-        if my.property.owner is None:
-            player.purchase(my.property)
-        elif my.property.owner != None:
-            player.payrent(my.property)
+    def effect(self, player):
+        if self.property.owner is None:
+            player.purchase(self.property)
+        elif self.property.owner != None:
+            player.payrent(self.property)
 
 #Randomly add or sub from the players money (either gain up to 200hkd or lose up to 300hkd)
 class Chance_Slot(Slot):
-    def effect(my, player):
+    def effect(self, player):
         addsub = random.choice(range(-300, 210, 10))
         player.money += addsub
 
 #Pay 10% of player total money
 class Tax_Slot(Slot):
-    def effect(my, player):
+    def effect(self, player):
         tax = (player.money // 10) * 10
         player.money -= tax
 
 class Free_Parking_Slot(Slot):
-    def effect(my, player):
+    def effect(self, player):
         pass
 
 class Visiting_Slot:
-    def effect(my, player):
+    def effect(self, player):
         pass
 
 class Gotojail_Slot(Slot):
-    def effect(my, player):
+    def effect(self, player):
         player.gojail()
 
 class Jail_Slot(Slot):
-    def double(my):
+    def double(self):
         return random.randint(1,6) == random.randint(1,6)
 
-    def throwdice(my):
+    def throwdice(self):
         dice1 = random.randint(1,6)
         dice2 = random.randint(1,6)
         dicesum = dice1 + dice2
         return dicesum
 
-    def effect(my, player):
+    def effect(self, player):
         if player.injail:
             player.jailturns += 1
             if player.jailturns <= 3 and player.money >= 150:
@@ -111,22 +111,22 @@ class Jail_Slot(Slot):
                 if decision == 'yes':
                     player.money -= 150
                     player.outjail()
-                    rolldice = my.throwdice
+                    rolldice = self.throwdice
                     player.move(rolldice)
             if player.jailturns == 3:
                 player.money -= 150
                 player.outjail()
-                rolldice = my.throwdice
+                rolldice = self.throwdice
                 player.move(rolldice)
-            elif my.double:
+            elif self.double:
                 player.outjail()
-                rolldice = my.throwdice
+                rolldice = self.throwdice
                 player.move(rolldice)
 
 #Board 
 class board:
-    def slots(my):
-        my.locations = [
+    def slots(self):
+        self.locations = [
             Goslot(1,"Go"),
             Property_Slot(2, "Central", 800, 90),
             Property_Slot(3, "Wan Chai", 700, 65),
@@ -149,34 +149,35 @@ class board:
             Property_Slot(20, "Tai O", 600, 25),
         ]
 
-    def myposition(my, position):
-        return my.locations[position]
+    def selfposition(self, position):
+        return self.locations[position]
     
 # Game Logic
 class Monopoly:
-    def start(my):
-        my.board = board()
-        my.players = []
-        my.current_position = 0
-        my.round_count = 0  # count round
+    def start(self):
+        self.board = board()
+        self.players = []
+        self.current_position = 0
+        self.round_count = 0  # count round
+        self.turn_count = 0
 
     # Add players
-    def add_player(my, name):
-        my. players.append(Player(name))
+    def add_player(self, name):
+        self. players.append(Player(name))
 
     # Roll dices
-    def roll_dice(my):
+    def roll_dice(self):
         dice1 = random.randint(1,6)
         dice2 = random.randint(1,6)
         return dice1, dice2
 
     # player roll 2 dices and move accordin to the sum of both dices    
-    def turns(my):
-        if len(my.players)>1 and my.round_count <100:
-            player = my.players[my.current_position]
+    def turns(self):  # main game playing logic/
+        if len(self.players)>1 and self.round_count <100:
+            player = self.players[self.current_position]
             print(player.name + "'s turn")
         
-            dice1, dice2 = my.roll_dice()
+            dice1, dice2 = self.roll_dice()
             print("First dice is " + dice1)
             print("First dice is " + dice2)
 
@@ -186,20 +187,22 @@ class Monopoly:
 
             #if players money is less than 0, gets eliminated
             if player.money < 0:
-                my.players.remove(player)
+                self.players.remove(player)
                 print("Player Eliminated")
         
-            my.current_position = (my.current_position + 1) % len(my.players)
-            my.round_count +=1 # not sure if it is right to add riaht here, check again first
+            self.current_position = (self.current_position + 1) % len(self.players)
+            self.turn_count +=1 # not sure if it is right to add riaht here, check again first
         else:
-            my.end_game
+            self.end_game()
     
-    def end_game(my):
-        if len(my.players) == 1:
-            print(f"The game ended! {my.players[0].name} wins!")
+    def end_game(self):
+        if len(self.players) == 1:
+            print(f"The game ended! {self.players[0].name} wins!")
         else:
             print("Game ended after 100 rounds.")
-        sys.exit()
+        user_input = input("Press Enter to exit: ")
+        if user_input == "":
+            sys.exit()
         
 
 
@@ -219,8 +222,8 @@ def Start_game(board_instance: board, monopoly_game: Monopoly):
             break
         
         # loop until only 1 player is left
-        while len(monopoly_game.players) > 1 and monopoly_game.round_count < 100: 
-            monopoly_game.play_turn()
+        while True: 
+            monopoly_game.turns()
 
-        if len(monopoly_game.players) == 1:
-            print(f"The game ended! {monopoly_game.players[0].name} wins!")
+        # if len(monopoly_game.players) == 1:
+        #     print(f"The game ended! {monopoly_game.players[0].name} wins!")
